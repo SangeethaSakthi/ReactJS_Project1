110d4b5033859952c232335d7717440316c478c0
Sangeetha <sangeetha.sakthivel@oracle.com>
2023-04-12 16:35:02 +0530

    Functional component ReactApp

---
+++ src/Components/Greet_FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/* function FirstGreet(){
+    return <h1>Hi reactjs</h1>
+} */
+
+const greet = () => <h1>Hello reactjs</h1>;
+
+//export default FirstGreet;
+
+export default greet
